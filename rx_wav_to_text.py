b80520e993421695cd38a521cf411f54d942c542
GuyEshel <134394173+eshelguy@users.noreply.github.com>
2025-06-25 00:31:26 +0300

    Delete rx_wav_to_text.py

--- rx_wav_to_text.py
+++
@@ -1,9 +0,0 @@
-from afskmodem import Receiver
-
-def wav_to_text(filename='output.wav', baud=1200):
-    rx = Receiver(baud)
-    result = rx.load(filename, string=True)
-    print(f"[✓] Decoded message: {result}")
-
-if __name__ == "__main__":
-    wav_to_text()
